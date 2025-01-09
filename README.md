405873419d5c4c43132f2528bad2cf2a0440589a
YusufOzmen01 <yusufmarquina@gmail.com>
2025-01-09 23:01:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# VRoid Hub Loader for DesktopMate
+
+This mod unlocks the hidden VRoid Hub menu inside of DesktopMate and copies some parameters and adjusts the application UI to help the user easily change the model and transfer animations!
+
+*Currently only transfers the animations of the default character. Support for transferring Miku's animations will be added if I ever buy the DLC and reverse engineer that*
+
+## Installation
+- Install the latest [MelonLoader](https://github.com/LavaGang/MelonLoader/releases/download/v0.6.6/MelonLoader.Installer.exe) version to DesktopMate (v0.6.6 is the recommended one)
+- Run DesktopMate once (and make sure there is a command prompt application popping up)
+- Close DesktopMate and download the [VroidHubLoader.dll]([https:](https://github.com/YusufOzmen01/desktopmate-vroidhub-mod/releases/download/v1.0.0/VroidHubLoader.dll)) file from here or the releases and put it into the Mods file inside of DesktopMate
+
+## [Click here for a video tutorial on how to use the mod]()
+
+## How to use 
+TO BE WRITTEN, WATCH THE VIDEO FOR NOW
+
+# License
+Check the [LICENSE.md](LICENSE.md) file for details
---
+++ VRoidHubLoader/Core.cs
@@ -0,0 +1,83 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Il2Cpp;
+using Il2CppKirurobo;
+
+[assembly: MelonInfo(typeof(MelonLoaderMod1.Core), "VRoid Hub Loader Mod", "1.0.0", "SergioMarquina", null)]
+[assembly: MelonGame("infiniteloop", "DesktopMate")]
+
+namespace MelonLoaderMod1
+{
+    public class Core : MelonMod
+    {
+        CharaData charaData;
+        GameObject chara;
+        RuntimeAnimatorController runtimeAnimatorController;
+
+        bool cloned = false, replaced = false;
+
+        public override void OnUpdate()
+        {
+            if (!cloned)
+            {
+                chara = GameObject.Find("iltan_prefab(Clone)");
+                if (chara != null)
+                {
+                    charaData = chara.GetComponent<CharaData>();
+                    runtimeAnimatorController = chara.GetComponent<Animator>().runtimeAnimatorController;
+
+                    LoggerInstance.Msg("Chara copied! Removing default chara...");
+                    chara.SetActive(false);
+                    cloned = true;
+
+                    UnityEngine.Object.Destroy(chara);
+                    Thread.Sleep(1000);
+                    GameObject.Find("/MenuCanvas/MenuParent/Old").SetActive(true);
+                    GameObject.Find("/MenuCanvas/MenuParent/Old/ModelPageOld").SetActive(true);
+
+                    GameObject.Find("/UniWindowController").GetComponent<UniWindowController>().isTransparent = false;
+                    Screen.SetResolution(800, 600, false);
+                    LoggerInstance.Msg("VRoid Hub menu enabled!");
+                }
+
+                return;
+            }
+
+            if (!replaced)
+            {
+                chara = GameObject.Find("VRM1");
+                if (chara != null)
+                {
+                    CharaData newCharaData = chara.AddComponent<CharaData>();
+                    newCharaData.alarmAnim = charaData.alarmAnim;
+                    newCharaData.draggedAnims = charaData.draggedAnims;
+                    newCharaData.hideLeftAnims = charaData.hideLeftAnims;
+                    newCharaData.hideRightAnims = charaData.hideRightAnims;
+                    newCharaData.jumpInAnim = charaData.jumpInAnim;
+                    newCharaData.jumpOutAnim = charaData.jumpOutAnim;
+                    newCharaData.pickedSittingAnim = charaData.pickedSittingAnim;
+                    newCharaData.pickedStandingAnim = charaData.pickedStandingAnim;
+                    newCharaData.sittingOneShotAnims = charaData.sittingOneShotAnims;
+                    newCharaData.sittingRandomAnims = charaData.sittingRandomAnims;
+                    newCharaData.standingOneShotAnims = charaData.standingOneShotAnims;
+                    newCharaData.standingRandomAnims = charaData.standingRandomAnims;
+                    newCharaData.strokedSittingAnim = charaData.strokedSittingAnim;
+                    newCharaData.strokedStandingAnim = charaData.strokedStandingAnim;
+
+                    Animator charaAnimator = chara.GetComponent<Animator>();
+                    charaAnimator.applyRootMotion = true;
+                    charaAnimator.cullingMode = AnimatorCullingMode.CullUpdateTransforms;
+                    charaAnimator.runtimeAnimatorController = runtimeAnimatorController;
+
+                    LoggerInstance.Msg("Chara replaced!");
+                    Thread.Sleep(1000);
+                    GameObject.Find("/MenuCanvas/MenuParent/Old").SetActive(false);
+                    GameObject.Find("/UniWindowController").GetComponent<UniWindowController>().isTransparent = true;
+                    LoggerInstance.Msg("UI fixed!");
+
+                    replaced = true;
+                }
+            }
+        }
+    }
+}
